869da2b40ef9bb5cd2751b11ae0705c96faf996a
RimaSem <rima.semetaite@gmail.com>
2022-11-14 15:51:12 +0200

    Create promptPlayer and game functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get random computer choice: rock, paper or scissors
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     switch (choice) {
@@ -14,10 +15,13 @@ function getComputerChoice() {
     return choice;
 }
 
+// Format player choice, so that it is in lowercase with first capitalized letter
 function formatText(str) {
     return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+
+// Play one round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
     const player = formatText(playerSelection);
     const youWinText = `You Win! ${player} beats ${computerSelection}`;
@@ -49,6 +53,48 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// Player input validation
+function promptPlayer() {
+    while (true) {
+        let input = prompt("Choose rock, paper or scissors:");
+        input = formatText(input);
+        if (input === "Rock" || input === "Paper" || input === "Scissors") {
+            return input;
+        } else {
+            alert("Incorrect input. Try again");
+        }
+    }
+}
+
+// Play main game consisting of 5 rounds
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult = "";
+    let finalMessage = "It's a tie!"
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = promptPlayer();
+        const computerSelection = getComputerChoice();
+        roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult.includes("You Win")) {
+            playerScore++;
+        } else if (roundResult.includes("You Lose")) {
+            computerScore++;
+        }
+
+        console.log(`${playerScore}:${computerScore}. ${roundResult}`);
+    }
+
+    if (playerScore > computerScore) {
+        return `Player: ${playerScore}, Computer: ${computerScore}. You Win!`
+    } else if (playerScore < computerScore) {
+        return `Player: ${playerScore}, Computer: ${computerScore}. You Lose!`
+    }
+
+    return finalMessage;
+}
+
+
+console.log(game());
