dcde0a3f7dd22672e67a5e1b00035027a907e451
RimaSem <rima.semetaite@gmail.com>
2022-11-19 18:28:37 +0200

    Improve styling and clean up code

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const buttons = document.querySelectorAll("img");
+const resetBtn = document.querySelector("button");
+const playerScore = document.querySelector("#p-scr");
+const computerScore = document.querySelector("#c-scr");
+const score = document.querySelector(".score-title");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", game);
+});
+
+resetBtn.addEventListener("click", resetGame);
+
 // Get random computer choice: rock, paper or scissors
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
@@ -14,11 +26,6 @@ function getComputerChoice() {
   }
   return choice;
 }
-
-// // Format player choice, so that it is in lowercase with first capitalized letter
-// function formatText(str) {
-//   return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
-// }
 
 // Play one round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
@@ -51,56 +58,22 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// Play main game consisting of 5 rounds
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     let roundResult = "";
-//     let finalMessage = "It's a tie!"
-
-//     for (let i = 0; i < 5; i++) {
-//         const playerSelection = promptPlayer();
-//         const computerSelection = getComputerChoice();
-//         roundResult = playRound(playerSelection, computerSelection);
-
-//         if (roundResult.includes("You Win")) {
-//             playerScore++;
-//         } else if (roundResult.includes("You Lose")) {
-//             computerScore++;
-//         }
-
-//         console.log(`${playerScore}:${computerScore}. ${roundResult}`);
-//     }
-
-//     if (playerScore > computerScore) {
-//         return `Player: ${playerScore}, Computer: ${computerScore}. You Win!`
-//     } else if (playerScore < computerScore) {
-//         return `Player: ${playerScore}, Computer: ${computerScore}. You Lose!`
-//     }
-
-//     return finalMessage;
-// }
-
-const buttons = document.querySelectorAll("img");
-const playerScore = document.querySelector("#p-scr");
-const computerScore = document.querySelector("#c-scr");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", game);
-});
-
+// play 5 rounds of rock, paper, scissors
 function game(e) {
-  const score = document.querySelector(".score-title");
   const text = playRound(e.target.className, getComputerChoice());
 
   if (computerScore.textContent == 5 || playerScore.textContent == 5) return;
 
   if (text.includes("You Win!") && playerScore.textContent == 4) {
     ++playerScore.textContent;
-    score.textContent = "~~~ YOU ARE THE WINNER! ~~~";
+    score.classList.toggle("larger-text");
+    score.textContent = "YOU WON THE GAME! YAY!";
+    resetBtn.style.display = "initial";
   } else if (text.includes("You Lose!") && computerScore.textContent == 4) {
     ++computerScore.textContent;
+    score.classList.toggle("larger-text");
     score.textContent = "YOU LOST THE GAME! :(";
+    resetBtn.style.display = "initial";
   } else {
     if (text.includes("You Win!")) {
       ++playerScore.textContent;
@@ -112,4 +85,12 @@ function game(e) {
       score.textContent = text;
     }
   }
+}
+
+function resetGame() {
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  score.classList.toggle("larger-text");
+  score.textContent = "Score";
+  resetBtn.style.display = "none";
 }
